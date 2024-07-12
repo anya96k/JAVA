32cf1ae76d539330fba674b07576a4892bbb4d9f
anya96k <161941144+anya96k@users.noreply.github.com>
2024-07-11 22:34:14 -0700

    Add files via upload

---
+++ pd.java
@@ -0,0 +1,25 @@
+class pd {
+   
+    static int reversNumber(int n)
+    {
+        int reversed_n = 0;
+        while (n > 0) {
+            reversed_n = reversed_n * 10 + n % 10;
+            n = n / 10;
+        }
+        return reversed_n;
+    }
+ 
+     public static void main(String[] args)
+    {
+        int n = 123464321;
+        int reverseN = reversNumber(n);
+        System.out.println("Reverse of n = " + reverseN);
+ 
+        
+        if (n == reverseN)
+            System.out.println("Palindrome = Yes");
+        else
+            System.out.println("Palindrome = No");
+    }
+}
