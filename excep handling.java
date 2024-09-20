64922b85a684c533e1e0766b784735029fef631e
Aniket Shinde <161941144+anya96k@users.noreply.github.com>
2024-09-19 22:18:49 -0700

    Add files via upload

---
+++ excep handling.java
@@ -0,0 +1,21 @@
+class demo{|
+public static void main(Strings args[])
+System.out.println("the program is :");
+int a=10,b=0,c;
+try{
+    if(b!=0)
+    {
+        c=a/b;
+        System.out.println(c);
+    
+    }
+    else{
+        System.out.println("cannot divide by zero:");
+    }
+    catch(Exception e)
+    {
+        System.out.println("something went wrong:");
+    }
+}
+
+}
