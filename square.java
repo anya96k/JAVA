aa10f991012e1ac564ddebb076e62b6af46b3259
Aniket Shinde <161941144+anya96k@users.noreply.github.com>
2025-01-20 16:41:04 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,15 @@
+public class square
+{
+
+ public static void main(String[] args)
+ {
+   	double side=10;
+	double area=side*side;
+	double perimeter=2*side;	
+	System.out.println("side of square:" + side);
+	System.out.println("area of square:" + area);
+
+	System.out.println("perimeter  of square:" + perimeter);
+
+ }
+	}
