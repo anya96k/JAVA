64f4c3249fd3a5470641fc68a3bbea74aa7933ef
Aniket Shinde <161941144+anya96k@users.noreply.github.com>
2025-01-20 16:40:49 +0530

    Add files via upload

---
+++ program.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class program
+{
+ public static void main(String[] args)
+ {
+Scanner sc = new Scanner(System.in);
+System.out.println("1st value");
+Double a= sc.nextDouble();
+System.out.println("2nd value");
+Double b= sc.nextDouble();
+System.out.println(Math.max(a,b));
+}
+}
